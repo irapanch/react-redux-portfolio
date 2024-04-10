d8b3a6b56cf6a06d02899342e052db114a356f9c
irapanch <117355580+irapanch@users.noreply.github.com>
2024-04-10 17:17:00 +0300

    configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,21 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
 import rootReducer from "./rootReducer";
-import { todosReducer } from "./todoList/reduser";
+import { configureStore } from '@reduxjs/toolkit';
 
-const enhancer = devToolsEnhancer();
-// export const store = createStore(counterReduser, enhancer)
-export const store = createStore(rootReducer, enhancer)
-// export const store = createStore(todosReducer, enhancer)
+
+// рефакторинг редакс-тулкіт
+export const store = configureStore({
+    reducer: rootReducer,
+
+    // можемо закривати дані від користувачів й розробників напряму
+    // devTools: false,
+
+    // або перевіряти, щоб виключати девтулзи лише при продакшені
+    devTools: process.env.NODE_ENV !== 'production',
+})
+console.log(process.env);
+// const enhancer = devToolsEnhancer();
+// // export const store = createStore(counterReduser, enhancer)
+// export const store = createStore(rootReducer, enhancer)
+// // export const store = createStore(todosReducer, enhancer)
